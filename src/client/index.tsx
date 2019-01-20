b22364bb52b1f77b885806bb4e4d24674e5c4156
ElectronicManuel <23333606+ElectronicManuel@users.noreply.github.com>
2019-01-20 04:34:54 +0100

    fixing connection

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
     render() {
         return (
-            <p>Test 2</p>
+            <div>
+                <p>Test 2</p>
+                <button onClick={async () => {
+                    try {
+                        const result = await axios.get('/test');
+                        alert(result.data);
+                    } catch(err) {
+                        alert(JSON.stringify(err));
+                    }
+                }}>Request</button>
+            </div>
         )
     }
 }
